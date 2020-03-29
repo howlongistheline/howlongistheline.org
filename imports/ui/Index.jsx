4ecbfb1abaa4c4107bb61e878f0fc4453012fa0b
ioioio8888 <louislui9511@gmail.com>
2020-03-29 16:56:39 +0800

    Problem: Not enough space to display info
    Solution: change from list to card

--- imports/ui/Index.jsx
+++ imports/ui/Index.jsx
@@ -3,48 +3,56 @@ import MainLayout from './MainLayout'
 import { withTracker } from 'meteor/react-meteor-data';
 import { locations } from '../api/lines.js';
 import { Meteor } from 'meteor/meteor';
-import { Icon, Button, ListItem, ListTitle } from 'react-onsenui'
+import { Icon, Button, ListItem, ListTitle, Card } from 'react-onsenui'
 import moment from 'moment';
 
-function Index({locations, history}) {
+function Index({ locations, history }) {
 
-    function statusToWord(statusCode){
-        switch(statusCode){
+    function statusToWord(statusCode) {
+        switch (statusCode) {
             case "no":
-                return <div style={{color:"green"}}>No Lines!</div>
+                return <div style={{ color: "green" }}>No Lines!</div>
             case "small":
-                return <div style={{color:"orange"}}>A Wee Wait</div>
+                return <div style={{ color: "orange" }}>A Wee Wait</div>
             case "long":
-                return <div style={{color:"red"}}>Busy. Stay Home.</div>
+                return <div style={{ color: "red" }}>Busy. Stay Home.</div>
         }
     }
 
     function renderList() {
         return locations.map((location) => {
             return (
-            <ListItem key={location._id} tappable onClick={()=>{
-                history.push('/editLine?id='+location._id)
-            }}>
-                //<div className="left">{location.name}</div>
-                <div className="left">{location.address}</div>
-                <div className="center">Last updated: {moment(location.lastUpdate).fromNow()}</div>
-                <div className="right">Waiting time:&nbsp;{statusToWord(location.status)}</div>
-            </ListItem>)
+                <Card key={location._id} tappable onClick={() => {
+                    history.push('/editLine?id=' + location._id)
+                }}>
+                    <ListTitle>
+                    {location.name}{location.address}
+                    </ListTitle>
+                    <ListItem>
+                    {location.address}
+                    </ListItem>
+                    <ListItem>
+                        Last updated: {moment(location.lastUpdate).fromNow()}
+                    </ListItem>
+                    <ListItem>
+                        Waiting time:&nbsp;{statusToWord(location.status)}
+                    </ListItem>
+                </Card>)
         })
     }
 
     return (
         <MainLayout>
-                <ListTitle>
-                    Shops Near You
+            <ListTitle>
+                Shops Near You
                 </ListTitle>
-                {renderList()}
-                <Button modifier="large--cta" style={{ position: "fixed", bottom: 0, zIndex: 1000, minHeight: 50 }}
-                    // type="submit"
-                    onClick={() => {history.push('/addLine')}}>
-                    Add a new shop
-                    <Icon style={{marginLeft: 10}} icon='fa-plus' />
-                </Button>
+            {renderList()}
+            <Button modifier="large--cta" style={{ position: "fixed", bottom: 0, zIndex: 1000, minHeight: 50 }}
+                // type="submit"
+                onClick={() => { history.push('/addLine') }}>
+                Add a new shop
+                    <Icon style={{ marginLeft: 10 }} icon='fa-plus' />
+            </Button>
         </MainLayout>
     )
 }
@@ -54,7 +62,7 @@ export default withTracker(() => {
     Meteor.subscribe('locations');
 
     return {
-      locations: locations.find({}, { sort: { lastUpdate: -1 } }).fetch(),
-    //   currentUser: Meteor.user,
+        locations: locations.find({}, { sort: { lastUpdate: -1 } }).fetch(),
+        //   currentUser: Meteor.user,
     };
-  })(Index);
+})(Index);
