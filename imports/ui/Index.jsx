3dba25857996f5f6f098ee1ea85caa22a83a8832
ioioio8888 <louislui9511@gmail.com>
2020-03-29 22:01:08 +0800

    Problem: no search function
    Solution: implement search function

--- imports/ui/Index.jsx
+++ imports/ui/Index.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import MainLayout from './MainLayout'
 import { withTracker } from 'meteor/react-meteor-data';
-import { locations } from '../api/lines.js';
+import { locations, locationsIndex } from '../api/lines.js';
 import { Meteor } from 'meteor/meteor';
-import { Icon, Button, ListItem, ListTitle, Card, ProgressCircular } from 'react-onsenui'
+import { Icon, Button, ListItem, ListTitle, Card, ProgressCircular, SearchInput } from 'react-onsenui'
 import moment from 'moment';
+import { Tracker } from 'meteor/tracker'
 import { toast } from 'react-toastify';
 
-function Index({ AllLocations, history }) {
-    
-    const [nearby ,setNearby]=useState();
+function Index({ history }) {
+
+    const [nearby, setNearby] = useState();
+    const [AllLocations, setAllLocations] = useState([])
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
-            Meteor.call('location.findnearby',position.coords.longitude, position.coords.latitude, (err, result)=>{         
+            Meteor.call('location.findnearby', position.coords.longitude, position.coords.latitude, (err, result) => {
                 setNearby(result)
             })
         }, (err) => {
@@ -23,6 +26,32 @@ function Index({ AllLocations, history }) {
         return () => {
         }
     }, [])
+
+    useEffect(() => {
+        let isCancelled = false;
+        try {
+            if (!isCancelled) {
+                Tracker.autorun(function () {
+                    if(!isCancelled){
+                    if(search == ""){
+                        setAllLocations(locations.find({}, { sort: { lastUpdate: -1 }, limit: 20 }).fetch(),)
+                    }
+                    else{
+                        let cursor = locationsIndex.search(search)
+                        setAllLocations(cursor.fetch())
+                    }
+                }
+                })
+            }
+          } catch (e) {
+            if (!isCancelled) {
+              throw e;
+            }
+          }
+        return function cleanup(){
+            isCancelled = true;
+        }
+    }, [search])
 
 
     function statusToWord(statusCode) {
@@ -64,7 +93,7 @@ function Index({ AllLocations, history }) {
     }
 
     function renderNearby() {
-        if(!nearby){
+        if (!nearby) {
             return (
                 <Card>
                     <ProgressCircular indeterminate />
@@ -96,18 +125,49 @@ function Index({ AllLocations, history }) {
                 </Card>)
         })
     }
-
+    if(search != ""){
+        return (
+            <MainLayout>
+                <div style={{position: "sticky", top:0}}>
+                <ListItem>
+                        <SearchInput style={{ width: "80%" }} placeholder="search" onChange={(e)=>{
+                            setSearch(e.target.value)
+                        }}/>
+                </ListItem>
+                </div>
+                <div style={{ marginBottom: 55 }}>
+                <ListTitle>
+                    Results
+                </ListTitle>
+                    {renderList()}
+                </div>
+                <Button modifier="large--cta" style={{ position: "fixed", bottom: 0, zIndex: 1000, minHeight: 50 }}
+                    // type="submit"
+                    onClick={() => { history.push('/addLine') }}>
+                    Add a new shop
+                        <Icon style={{ marginLeft: 10 }} icon='fa-plus' />
+                </Button>
+            </MainLayout>
+        )
+    }
     return (
         <MainLayout>
+            <div style={{position: "sticky", top:0}}>
+            <ListItem>
+                    <SearchInput style={{ width: "80%" }} placeholder="search" onChange={(e)=>{
+                        setSearch(e.target.value)
+                    }}/>
+            </ListItem>
+            </div>
             <div style={{ marginBottom: 55 }}>
-            <ListTitle>
-                Shops Near You
+                <ListTitle>
+                    Shops Near You
             </ListTitle>
-            {renderNearby()}
-            <ListTitle>
-                All Shops
+                {renderNearby()}
+                <ListTitle>
+                    All Shops
             </ListTitle>
-            {renderList()}
+                {renderList()}
             </div>
             <Button modifier="large--cta" style={{ position: "fixed", bottom: 0, zIndex: 1000, minHeight: 50 }}
                 // type="submit"
@@ -124,7 +184,7 @@ export default withTracker(() => {
     Meteor.subscribe('locations');
 
     return {
-        AllLocations: locations.find({}, { sort: { lastUpdate: -1 } }).fetch(),
+        // AllLocations: locations.find({}, { sort: { lastUpdate: -1 } }).fetch(),
         //   currentUser: Meteor.user,
     };
 })(Index);
