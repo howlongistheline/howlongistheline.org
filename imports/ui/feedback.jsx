1b59bb4e945814c7ec4876e4d31fd611464d26fe
gazhayes <gareth.hayes@gmail.com>
2020-04-11 16:39:29 +1200

    problem: no community information

--- imports/ui/feedback.jsx
+++ imports/ui/feedback.jsx
@@ -5,56 +5,17 @@ import { toast } from 'react-toastify';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Locations } from '../api/lines.js';
 
-export default function feedback({ history, details }) {
-
-    const [feedback, setFeedback] = useState("");
-
-    function submit() {
-
-        if (feedback == "") {
-            toast("Please Enter your feedback");
-            return
-        }
-            Meteor.call('feedbacks.insert', feedback, function (err, result) {
-                if (err) {
-                    toast("Such error! If this keeps happening please report it.")
-                    console.log(err)
-                    return
-                }
-                // setLoading(false)
-                toast("Thank You for your feedback!")
-                history.push('/')
-            });
-    }
+export default function feedback({ }) {
 
     return (
         <MainLayout>
-            <div style={{ marginBottom: 55 }}>
             <ListTitle>
                 Feedback
             </ListTitle>
             <ListItem>
-                <p>Problems that are currently known and being worked on can be seen at <a href="https://github.com/howlongistheline/howlongistheline.org/issues">https://github.com/howlongistheline/howlongistheline.org/issues</a></p>
+                <p>Problems (and new features) that are currently known and being worked on can be seen at <a href="https://github.com/howlongistheline/howlongistheline.org/issues">https://github.com/howlongistheline/howlongistheline.org/issues</a></p>
+                <p><a href="https://www.facebook.com/groups/1161156860891990/">Facebook group</a> that some of the volunteer developers sometimes hang around in.</p>
             </ListItem>
-                 <ListItem>
-
-
-                <Input
-                        style={{ width: "80%" }}
-                        value={feedback} float
-                        onChange={(event) => { setFeedback(event.target.value) }}
-                        modifier='material'
-                        placeholder='Please leave your comment here and press Submit below. Leave your email address if you would like a reply.' />
-            </ListItem>
-            </div>
-            <Button modifier="large--cta" style={{ position: "fixed", bottom: 0, zIndex: 1000, minHeight: 50 }}
-                // type="submit"
-                onClick={() => {
-                    submit()
-                }}>
-                Submit
-                    <Icon style={{ marginLeft: 10 }} icon='fa-plus' />
-            </Button>
         </MainLayout>
     )
 }
