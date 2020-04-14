64388a167e3ad2c71747b34ed0d9b05046dc14f4
ioioio8888 <louislui9511@gmail.com>
2020-04-14 14:11:47 +0800

    Problem: no way to add stock status
    Solution: add a stock status page to let user add/check the stock status

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -12,6 +12,7 @@ import learntocode from '../../ui/learntocode'
 import Nearme from '../../ui/nearme'
 import faq from '../../ui/FAQ'
 import duplicated from '../../ui/Duplicated'
+import Stocks from '../../ui/Stocks'
 import { ToastContainer } from 'react-toastify';
 import { CookiesProvider } from 'react-cookie';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
@@ -37,6 +38,7 @@ export const renderRoutes = () => (
         <Route exact path="/FAQ" component={faq} />
         <Route exact path="/learntocode" component={learntocode} />
         <Route exact path="/duplicated" component={duplicated} />
+        <Route exact path="/stocks" component={Stocks} />
       </Switch>
       </CookiesProvider>
       </MuiPickersUtilsProvider>
